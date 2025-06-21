db75331218704cde01c974b44df3f25111472ba5
Vinny <vincegenov7@gmail.com>
2025-06-21 14:24:57 -0400

    added weather, brought back api keys

--- info.py
+++ info.py
@@ -2,15 +2,15 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from collections import Counter
 from datetime import datetime, timedelta
-import os
+# import os
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # USER CLIENT: to get songs they are listening to and stuff
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
-    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+    client_id="b03e39464f6549d0a8b182994115dc73",
+    client_secret="bb2e540b1e1d418d8a0007ac92685422",
     redirect_uri="http://127.0.0.1:8888/callback",
     scope="user-read-recently-played"
 ))
@@ -38,4 +38,67 @@ def getGenres(sp, date):
     return dict(genre_counter)
 
 
-print(getGenres(sp, datetime.now().date()))
+# print(getGenres(sp, datetime.now().date()))
+
+# LOCATION GATHERING
+import requests
+
+def get_location():
+    try:
+        response = requests.get("https://ipinfo.io/json")
+        data = response.json()
+        location = data.get("loc", "0,0").split(",")
+        city = data.get("city", "")
+        region = data.get("region", "")
+        country = data.get("country", "")
+        return {
+            "latitude": location[0],
+            "longitude": location[1],
+            "city": city,
+            "region": region,
+            "country": country
+        }
+    except Exception as e:
+        print("Error getting location:", e)
+        return None
+    
+
+# Weather condition mapping
+weather_map = {
+    0: "Clear sky",
+    1: "Mainly clear",
+    2: "Partly cloudy",
+    3: "Overcast",
+    45: "Fog",
+    48: "Depositing rime fog",
+    51: "Light drizzle",
+    53: "Moderate drizzle",
+    55: "Dense drizzle",
+    61: "Slight rain",
+    63: "Moderate rain",
+    65: "Heavy rain",
+    71: "Slight snow",
+    73: "Moderate snow",
+    75: "Heavy snow",
+    80: "Rain showers",
+    81: "Heavy rain showers",
+    82: "Violent rain showers",
+    95: "Thunderstorm",
+    96: "Thunderstorm with slight hail",
+    99: "Thunderstorm with heavy hail"
+}
+
+loc = get_location()
+if loc:
+    url = f"https://api.open-meteo.com/v1/forecast"
+    params = {
+    "latitude": loc['latitude'],
+    "longitude": loc['longitude'],
+    "start_date": datetime.now().strftime('%Y-%m-%d'),
+    "end_date": datetime.now().strftime('%Y-%m-%d'),
+    "daily": "weathercode",
+    "timezone": "America/Toronto"
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    print(data)
